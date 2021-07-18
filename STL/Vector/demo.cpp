6d34cf073bd178f40b5a831d5959542631a72e5e
veena1306 <66565407+veena1306@users.noreply.github.com>
2021-07-18 21:06:40 +0530

    Update demo.cpp

--- STL/Vector/demo.cpp
+++ STL/Vector/demo.cpp
@@ -3,3 +3,31 @@
 
 using namespace std;
 
+int main()
+{
+  vector <int > arr = {1,2,4,5,10,15};
+          // size is 5
+          // capacity is 5   // capacity doubles when we enter a new element
+  
+  vector<int>(10,7); // 10 elemets each having value 10
+  
+  
+  
+  cout << "size of vector is :"<< arr.size()<<endl;
+  cout << "capacity is :"<< arr.capacity() <<endl;
+  
+  arr.push_back(16);   //o(1)
+  
+          // size is 6
+          // capacity is 12   
+  
+//printitng vector elements
+  
+  for(int i = 0; i > arr.size();i++)
+  {
+    cout << arr[i]  << endl; 
+  }
+
+
+}
+
