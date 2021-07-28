194ffe732eecfb35031e127cbb819d081b91dab0
veena1306 <66565407+veena1306@users.noreply.github.com>
2021-07-28 17:21:50 +0530

    Create simple_example.cpp

---
+++ Static_Casting/simple_example.cpp
@@ -0,0 +1,39 @@
+/******************************************************************************
+
+                              Online C++ Debugger.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Debug" button to debug it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class parent
+{
+    
+};
+
+class brother : public parent
+{
+    
+};
+
+class sister : public parent
+{
+    
+};
+int main()
+{
+    float value = 3.19;
+    cout << int(value) <<endl;
+    cout << static_cast<int>(value) << endl;
+}
+   
+
+
+
+
+
