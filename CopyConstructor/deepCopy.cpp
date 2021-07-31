251f7f61d27a17385dc66dcc65fb5abfe3e4917b
veena1306 <66565407+veena1306@users.noreply.github.com>
2021-07-31 13:45:35 +0530

    Create deepCopy.cpp

---
+++ CopyConstructor/deepCopy.cpp
@@ -0,0 +1,6 @@
+/* It dynamically allocates memory for the copy first and then copies the actual value. 
+In a deep copy, both objects which have to copy and another which has to be copied will be having different memory locations. 
+So, the changes made to one will not affect another. 
+This is used by a user-defined copy constructor.  */
+
+
