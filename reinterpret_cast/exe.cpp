a78fc30859188ebb00e2d8dc3c27775acf34858a
veena1306 <66565407+veena1306@users.noreply.github.com>
2021-08-08 23:02:16 +0530

    Update exe.cpp

--- reinterpret_cast/exe.cpp
+++ reinterpret_cast/exe.cpp
@@ -31,3 +31,21 @@ int main()
 }
   
   
+=====================================================================================================================================================
+
+
+// CPP program to demonstrate working of 
+// reinterpret_cast
+#include <iostream>
+using namespace std;
+  
+int main()
+{
+    int* p = new int(65);
+    char* ch = reinterpret_cast<char*>(p);
+    cout << *p << endl;
+    cout << *ch << endl;
+    cout << p << endl;
+    cout << ch << endl;
+    return 0;
+}
